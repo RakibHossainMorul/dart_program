f4d7a3d70a689fdbecb86383ff5d0feb6498351b
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2023-04-24 18:00:56 +0600

    added

--- callback_function.dart
+++ callback_function.dart
@@ -1,9 +1,22 @@
+//Example 1
 void myFunction(int x, Function(int) callback) {
   // Perform some processing
   int result = x * 2;
 
   // Call the callback function with the result
   callback(result);
+}
+//Example 2
+
+void performTask(String message, Function callback) {
+  print('Task started: $message');
+  // do some time-consuming operation here
+  print('Task finished: $message');
+  callback();
+}
+
+void callbackFunction() {
+  print('Callback function called');
 }
 
 void main() {
@@ -14,4 +27,6 @@ void main() {
 
   // Call myFunction with the callback function
   myFunction(5, printResult);
+
+  performTask("Callback function example", callbackFunction);
 }
