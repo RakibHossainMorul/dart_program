f2a82cb84e87fd5654a21528a3b86d9cff9f657c
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2023-04-24 17:25:58 +0600

    updated

--- function_method.dart
+++ function_method.dart
@@ -7,9 +7,16 @@ int sum(int a, int b) {
 //Method is a block of code that can be called by an object/Class.
 class Person {
   late String name;
+  late int age;
 
   void sayHello() {
     print('Hello, my name is $name.');
+  }
+
+  void speak() {
+    var lastName = "James";
+    print("My name is $name and I am $age years old.");
+    print("Last name is $lastName");
   }
 }
 
@@ -17,5 +24,7 @@ void main() {
   print(sum(10, 20));
   var person = Person();
   person.name = 'James';
+  person.age = 30;
   person.sayHello();
+  person.speak();
 }
